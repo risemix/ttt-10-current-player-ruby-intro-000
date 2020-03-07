f11edbc0ea56a843dde831575700d3b2aaf5a737
Evan K Pattison <epattison@gmail.com>
2020-03-07 23:05:40 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,17 @@
+#Displays current state of the game board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts "-----------"
+end
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 def turn_count
+
 end
 
 def current_player
