ea1da4bf0d3ee988e4f11642914b7fa0b978fac6
Evan K Pattison <epattison@gmail.com>
2020-03-07 23:24:16 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -13,6 +13,8 @@ board = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
   turns = 0
   board.each do |space|
-
+    if space == "X" || "O"
+      turns += 1
+    end 
   end
 end
